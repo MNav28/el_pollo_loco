48d825fde626dc01be2ee09b75d096dc44c973eb
Michael Navarro <m.navarro@gmx.net>
2025-01-07 22:41:42 +0100

    add background elements to the canvas

--- models/world.class.js
+++ models/world.class.js
@@ -6,9 +6,12 @@ class World {
         new Chicken(),
         new Chicken(),
     ]
-    clouds = [      // eine cloud erstellen damit die angezeigt wird
+    clouds = [      
         new Cloud()
     ];  
+    backgroundObjects = [
+        new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 0) 
+    ];
     canvas; 
     ctx;            
 
@@ -20,20 +23,29 @@ class World {
     }
 
     draw() {  
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
+                                                                        
+
         
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
+        this.addObjectsToMap(this.backgroundObjects);       
+        this.addToMap(this.character);  
+        this.addObjectsToMap(this.clouds); 
+        this.addObjectsToMap(this.enemies);  
         
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
         
         let self = this;
         requestAnimationFrame(function() {  
             self.draw();   
         });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        })
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
