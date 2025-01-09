4b8e362b1501255dedaffff95321a811a4eb695d
Michael Navarro <m.navarro@gmx.net>
2025-01-09 22:02:21 +0100

    add more backgroundobjects, scaling the objekts and adding sky

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MoveableObject {
-
+    height = 280; 
+    width = 120;
+    y = 155; 
     constructor() {     
         super();         
         this.loadImage('./assets/img/2_character_pepe/1_idle/idle/I-1.png');
--- models/world.class.js
+++ models/world.class.js
@@ -10,7 +10,10 @@ class World {
         new Cloud()
     ];  
     backgroundObjects = [
-        new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 0) 
+        new BackgroundObject('./assets/img/5_background/layers/air.png', 0),
+        new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 0),
     ];
     canvas; 
     ctx;            
@@ -24,8 +27,6 @@ class World {
 
     draw() {  
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
-                                                                        
-
         
         this.addObjectsToMap(this.backgroundObjects);       
         this.addToMap(this.character);  
