898e3a02a93b1d771abcb698245448fd54781f5a
Michael Navarro <m.navarro@gmx.net>
2025-01-25 10:21:25 +0100

    moving character

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MoveableObject {
     height = 280;
     width = 120;
     y = 155;
+    speed = 10;
     IMAGES_WALKING = [
         './assets/img/2_character_pepe/2_walk/W-21.png',
         './assets/img/2_character_pepe/2_walk/W-22.png',
@@ -10,7 +11,7 @@ class Character extends MoveableObject {
         './assets/img/2_character_pepe/2_walk/W-25.png',
         './assets/img/2_character_pepe/2_walk/W-26.png'
     ];
-    world; 
+    world;
 
     constructor() {
         super();
@@ -20,15 +21,28 @@ class Character extends MoveableObject {
         this.animate();
     }
 
-    animate() {  
+    animate() {
+
+        setInterval(() => { 
+            if (this.world.keyboard.RIGHT) {  
+                this.x += this.speed; 
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed; 
+            }
+        }, 1000 / 60);  
+
+
         setInterval(() => {
-            if (this.world.keyboard.RIGHT) { 
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) { 
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50); 
     }
 
     jump() {
