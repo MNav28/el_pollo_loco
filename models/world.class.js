0212a49ee077d7d5367aa65f90a0c09e3f068704
Michael Navarro <m.navarro@gmx.net>
2025-02-19 20:47:39 +0100

    added collision detection

--- models/world.class.js
+++ models/world.class.js
@@ -1,43 +1,43 @@
 class World {
-    myTest = console.log('ein test');  
-    character = new Character();    
+    myTest = console.log('ein test');
+    character = new Character();
     // enemies = level1.enemies;
     // clouds = level1.clouds;  
     // backgroundObjects = level1.backgroundObjects;
-    
-    level = level1;  
-    canvas; 
-    ctx;    
-    keyboard; 
-    camera_x = 0;  
-    
+
+    level = level1;
+    canvas;
+    ctx;
+    keyboard;
+    camera_x = 0;
+
     constructor(canvas, keyboard) {
-        this.ctx = canvas.getContext('2d'); 
-        this.canvas = canvas;  
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.keyboard = keyboard;
-        this.draw(); 
+        this.draw();
         this.setWorld();
     }
 
-    setWorld() {                  
-        this.character.world = this;    
+    setWorld() {
+        this.character.world = this;
     }
 
-    draw() {  
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.translate(this.camera_x, 0)  
-        
-        this.addObjectsToMap(this.level.backgroundObjects);       
-        this.addToMap(this.character);  
+        this.ctx.translate(this.camera_x, 0)
+
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
-        this.addObjectsToMap(this.level.enemies);  
-        
+        this.addObjectsToMap(this.level.enemies);
+
         this.ctx.translate(-this.camera_x, 0);
-        
+
         let self = this;
-        requestAnimationFrame(function() {  
-            self.draw();   
+        requestAnimationFrame(function () {
+            self.draw();
         });
     }
 
@@ -47,20 +47,26 @@ class World {
         })
     }
 
-    addToMap(mo) {  
-        if(mo.otherDirection) { 
-            this.ctx.save();   
-            this.ctx.translate(mo.width, 0);  
-            this.ctx.scale(-1,1);  
-            mo.x = mo.x * -1;   
-                        
-        }  
-
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);  
-
-        if(mo.otherDirection) {  
+    addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
             mo.x = mo.x * -1;
-            this.ctx.restore();   
+
         }
-    }       
+
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+
+        this.ctx.beginPath();
+        this.ctx.lineWidth = '5';
+        this.ctx.strokeStyle = 'blue';
+        this.ctx.rect(mo.x, mo.y, mo.width, mo.height);  // die Koordinaten an unseren jeweiligen Objekt einfügen d.h die x und die y koordinaten des objektes einfügen
+        this.ctx.stroke();
+
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
+    }
 }
