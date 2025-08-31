e4058c423f3f9cf9d80f3e0ee99a58a03f5b78aa
Michael Navarro <m.navarro@gmx.net>
2025-08-31 20:23:40 +0200

    feat(ui): add start screen, menu buttons and update game start logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,70 +1,83 @@
 let canvas;
-let world;   
+let world;
 let keyboard = new Keyboard();
 
-function init(){  
-    
-    canvas = document.getElementById('canvas')
-    world = new World(canvas, keyboard);   
+function init() {
 
-    
-    console.log('My Character is', world.character);   
+    canvas = document.getElementById('canvas')
+    world = new World(canvas, keyboard);
+
+
+    console.log('My Character is', world.character);
 }
 
-window.addEventListener("keydown", (e) => { 
+
+function startGame() {
+    const startScreen = document.getElementById('start-screen');
+    const soundButton = document.getElementById('sound-button');
+
+    startScreen.classList.add('d-none');
+
+    soundButton.classList.remove('d-none');
+
+    init();
+}
+
+
+window.addEventListener("keydown", (e) => {
     //console.log(e)  
 
-    if(e.keyCode == 37) {
+    if (e.keyCode == 37) {
         keyboard.LEFT = true;
     }
 
-    if(e.keyCode == 39) {
+    if (e.keyCode == 39) {
         keyboard.RIGHT = true;
     }
 
-    if(e.keyCode == 38) {
+    if (e.keyCode == 38) {
         keyboard.UP = true;
     }
 
-    if(e.keyCode == 40) {
+    if (e.keyCode == 40) {
         keyboard.DOWN = true;
     }
 
-    if(e.keyCode == 32) {
+    if (e.keyCode == 32) {
         keyboard.SPACE = true;
     }
 
-    if(e.keyCode == 68) {
+    if (e.keyCode == 68) {
         keyboard.D = true;
     }
 })
 
 
-window.addEventListener("keyup", (e) => { 
+window.addEventListener("keyup", (e) => {
     //console.log(e)  
-                    
 
-    if(e.keyCode == 37) {
+
+    if (e.keyCode == 37) {
         keyboard.LEFT = false;
     }
 
-    if(e.keyCode == 39) {
+    if (e.keyCode == 39) {
         keyboard.RIGHT = false;
     }
 
-    if(e.keyCode == 38) {
+    if (e.keyCode == 38) {
         keyboard.UP = false;
     }
 
-    if(e.keyCode == 40) {
+    if (e.keyCode == 40) {
         keyboard.DOWN = false;
     }
 
-    if(e.keyCode == 32) {
+    if (e.keyCode == 32) {
         keyboard.SPACE = false;
     }
 
-    if(e.keyCode == 68) {
+    if (e.keyCode == 68) {
         keyboard.D = false;
     }
 })
