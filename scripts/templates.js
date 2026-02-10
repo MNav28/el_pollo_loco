ba9e90fa7b730abe8eed639b1b59298c1b3fcfee
Michael Navarro <m.navarro@gmx.net>
2026-02-10 22:41:21 +0100

    impressum content updated

--- scripts/templates.js
+++ scripts/templates.js
@@ -25,7 +25,9 @@ function getImpressumTemplate() {
         <p>Quelle: e-recht24.de Impressum-Generator</p>
         <p>Die Soundeffekte stammen von <a href="https://pixabay.com/sound-effects/" rel="nofollow" target="_blank">Pixabay</a> </p>
         <p>Das Hintergrundbild stammt von <a href="https://www.freepik.com" rel="nofollow" target="_blank">Freepik</a> </p>
-        <p>Die Icons kommen von <a href="https://fontawesome.com/start" rel="nofollow" target="_blank">Fontawesome</a> </p>
+        <p>Die Icons kommen von <a href="https://fontawesome.com/start" rel="nofollow" target="_blank">Fontawesome</a> und 
+            <a href="https://www.flaticon.com" rel="nofollow" target="_blank">Flaticon</a>
+        </p>
         </div>
     `;
 }
