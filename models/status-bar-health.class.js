5b1c86c7aebca1a6753898b40ab9d1f504025d4d
Michael Navarro <m.navarro@gmx.net>
2026-05-31 17:41:12 +0200

    fix: show empty health bar only when character energy reaches zero

--- models/status-bar-health.class.js
+++ models/status-bar-health.class.js
@@ -17,28 +17,31 @@ class StatusBarHealth extends DrawableObject {
 
 
     constructor() {
-        super();     
-        this.loadImages(this.IMAGES_STATUSBAR_HEALTH);    
-        this.setPercentage(100); 
+        super();
+        this.loadImages(this.IMAGES_STATUSBAR_HEALTH);
+        this.setPercentage(100);
     }
 
-    
+
     setPercentage(percentage) {
         this.percentage = percentage;
-        let path = this.IMAGES_STATUSBAR_HEALTH[this.getImageIndex()];  
+        let path = this.IMAGES_STATUSBAR_HEALTH[this.getImageIndex()];
         this.img = this.imageCache[path];
     }
 
+
     getImageIndex() {
-        if (this.percentage == 100) {
+        if (this.percentage === 100) {
             return 5;
-        } else if (this.percentage > 80) {
+        } else if (this.percentage >= 80) {
             return 4;
-        } else if (this.percentage > 60) {
+        } else if (this.percentage >= 60) {
             return 3;
-        } else if (this.percentage > 40) {
+        } else if (this.percentage >= 40) {
             return 2;
-        } else if (this.percentage > 20) {
+        } else if (this.percentage >= 20) {
+            return 1;
+        } else if (this.percentage > 0) {
             return 1;
         } else {
             return 0;
