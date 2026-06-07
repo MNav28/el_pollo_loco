7ff23a5e5a91b804be3fcb81940238e25caee8eb
Michael Navarro <m.navarro@gmx.net>
2026-06-07 18:32:28 +0200

    docs(Bottle): add JSDoc documentation

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,3 +1,6 @@
+/**
+ * Represents a collectible salsa bottle in the game world.
+ */
 class Bottle extends MoveableObject {
 
     SALSA_BOTTLE_IMAGES = [
@@ -5,6 +8,12 @@ class Bottle extends MoveableObject {
         './assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png', 
     ]
 
+    /**
+     * Creates a new bottle at the given x position
+     * and randomly selects one of the bottle images.
+     *
+     * @param {number} x - Horizontal position of the bottle.
+     */    
     constructor(x) {
         super();
         let randomIndex = Math.floor(Math.random() * this.SALSA_BOTTLE_IMAGES.length);
