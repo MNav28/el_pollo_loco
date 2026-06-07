a3bb572dbc0a8a9e4379a9cfd050f2042341d4d5
Michael Navarro <m.navarro@gmx.net>
2026-06-07 18:11:45 +0200

    docs(BackgroundObject): add JSDoc documentation

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,18 @@
+/**
+ * Represents a static background element in the game world.
+ * Background objects are used to build the scrolling scenery.
+ */
 class BackgroundObject extends MoveableObject{
 
     width = 720; 
     height = 480;  
+
+    /**
+     * Creates a new background object.
+     *
+     * @param {string} imagePath - Path to the background image.
+     * @param {number} x - Horizontal position of the background object.
+     */    
     constructor(imagePath, x) {  
         super().loadImage(imagePath);
         this.x = x;
