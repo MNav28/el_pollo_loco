5e5b0490edee0f818130b40684fba83be13d4b86
Michael Navarro <m.navarro@gmx.net>
2026-06-07 19:02:34 +0200

    docs(Cloud): add JSDoc documentation

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,3 +1,6 @@
+/**
+ * Represents a cloud that continuously moves across the background.
+ */
 class Cloud extends MoveableObject {
     width = 500;
     height = 250;
@@ -7,6 +10,13 @@ class Cloud extends MoveableObject {
         './assets/img/5_background/layers/4_clouds/2.png',
     ]
 
+    /**
+     * Creates a new cloud at the given position.
+     *
+     * @param {number} x - Horizontal position of the cloud.
+     * @param {number} y - Vertical position of the cloud.
+     * @param {number} [imageIndex=0] - Index of the cloud image to display.
+     */    
     constructor(x, y, imageIndex = 0) {
         super();
         this.loadImage(this.CLOUDS_FLOATING[imageIndex % this.CLOUDS_FLOATING.length]);
@@ -17,6 +27,11 @@ class Cloud extends MoveableObject {
         this.animate();
     }
 
+    /**
+     * Starts the cloud movement animation.
+     * Moves the cloud to the left and repositions it
+     * when it leaves the visible game area.
+     */    
     animate() {
         setInterval(() => {
             if (this.isStopped) return;
