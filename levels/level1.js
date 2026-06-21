c7a18eb52e31dc6f45a6077b2ade7a268100df12
Michael Navarro <m.navarro@gmx.net>
2026-06-21 07:53:16 +0200

    background extended

--- levels/level1.js
+++ levels/level1.js
@@ -63,6 +63,10 @@ function initLevel() {
             new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 719 * 4),
             new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 719 * 4),
             new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 719 * 4),
+            new BackgroundObject('./assets/img/5_background/layers/air.png', 719 * 5),
+            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/2.png', 719 * 5),
+            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/2.png', 719 * 5),
+            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/2.png', 719 * 5),
         ],
         [
             new Bottle(240),
